6cdb4357e8b97cdffa9e5d3508dc8bb85071a9c2
dwchung4 <dwchung4@naver.com>
2016-04-05 22:29:24 -0400

    minor changes in page directory

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
 	url(r'^', include('home.urls')),
