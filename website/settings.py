63b2fe1767f1be6d4601b1f0f00cc8719fbcdd1a
dwchung4 <dwchung4@naver.com>
2016-04-07 01:30:17 -0400

    try postgres

--- website/settings.py
+++ website/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wtfprinceton_db',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
